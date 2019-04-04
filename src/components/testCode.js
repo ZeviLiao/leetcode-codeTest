bdfe6de49fbe35ca5e0cf38471af9f9117807eaf
Zevi Liao <jointjm@gmail.com>
2019-04-04 15:53:08 +0800

    testing code.

--- src/components/testCode.js
+++ src/components/testCode.js
@@ -1,14 +1,5 @@
 var detectCapitalUse = function(word) {
-  let WORD = word.toUpperCase()
-  let _word = word.toLowerCase()
-  if (WORD === word) return true
-  if (_word === word) return true
-
-  let header = word[0];
-  let tail = word.substr(1)
-  let whole = header.toUpperCase() + tail.toLowerCase()
-  if (whole === word) return true
-  return false
+  return word
 }
 
 export default {
