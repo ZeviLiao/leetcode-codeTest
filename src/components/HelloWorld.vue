0e13a5f9e9399110db78496535343ac8aaa1ad49
Zevi Liao <jointjm@gmail.com>
2019-04-04 18:32:22 +0800

    vscode debug mode

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,98 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <table>
+      <tr>
+        <td>1st</td>
+        <td>
+          <input type="number" v-model.number="scores.firstSection">
+        </td>
+      </tr>
+      <tr>
+        <td>2st</td>
+        <td>
+          <input type="number" v-model.number="scores.twoSection">
+        </td>
+      </tr>
+      <tr>
+        <td>3st</td>
+        <td>
+          <input type="number" v-model.number="scores.threeSection">
+        </td>
+      </tr>
+      <tr>
+        <td>4st</td>
+        <td>
+          <input type="number" v-model.number="scores.fourSection">
+        </td>
+      </tr>
+      <tr>
+        <td>extend</td>
+        <td>
+          <input type="number" v-model.number="scores.extendSection">
+        </td>
+      </tr>
+      <tr>
+        <td>total</td>
+        <td>
+          <input type="number" v-model.number="total">
+        </td>
+      </tr>
+    </table>
+    <input type="button" value="submit" @click="submit">
   </div>
 </template>
 
 <script>
+import _ from 'lodash'
+
+function mathDistance (num1, num2) {
+  return num2 > num1 ? num2 - num1 : num1 - num2
+}
+
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  name: 'hello',
+  data () {
+    return {
+      scores: {
+        firstSection: 0,
+        twoSection: 0,
+        threeSection: 0,
+        fourSection: 0,
+        extendSection: 0
+      },
+      cloneScores: {}
+    }
+  },
+  computed: {
+    total () {
+      return this.scores.firstSection + this.scores.twoSection + this.scores.threeSection +
+        this.scores.fourSection + this.scores.extendSection
+    }
+  },
+  methods: {
+    submit () {
+      let scores = this.scores
+      let modifedObject = _.reduce(this.cloneScores, function (result, value, key) {
+        return _.isEqual(value, scores[key]) ? result : result.concat(key)
+      }, [])
+      let isDistanceScore = false
+      modifedObject.forEach(property => {
+        if (mathDistance(this.cloneScores[property], scores[property]) > 1) {
+          isDistanceScore = true
+        }
+      })
+
+      if (isDistanceScore) alert('只能增加或減少一分')
+      if (modifedObject.length > 1) alert('只能更新一節的分數')
+    }
+  },
+  created () {
+    this.cloneScores = { ...this.scores }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+
 </style>
